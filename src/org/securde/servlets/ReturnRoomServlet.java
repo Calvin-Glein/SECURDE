2f61ec0012e116d48c7f26a88a8f43ea0ea02bd7
hankyungju <hankyungju@yahoo.com>
2017-08-10 10:11:26 +0900

    Integrated room reservation view

--- src/org/securde/servlets/ReturnRoomServlet.java
+++ src/org/securde/servlets/ReturnRoomServlet.java
@@ -1,34 +1,38 @@
 package org.securde.servlets;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.securde.beans.Room;
+import org.securde.services.MaterialServices;
 import org.securde.services.RoomServices;
+
 /**
- * Servlet implementation class ViewRoomsReserved
+ * Servlet implementation class ReturnRoomServlet
  */
-@WebServlet("/ViewRoomsReserved")
-public class ViewRoomsReserved extends HttpServlet {
+@WebServlet("/ReturnRoomServlet")
+public class ReturnRoomServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ViewRoomsReserved() {
+    public ReturnRoomServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doPost(request, response);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
@@ -36,10 +40,12 @@ public class ViewRoomsReserved extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		ArrayList<Room> reservations = RoomServices
-				.BorrowedByAccount(Integer.parseInt((request.getSession(false).getAttribute("accountID").toString())));
-		request.setAttribute("rooms", reservations);
-		request.getRequestDispatcher("/WEB-INF/jsp/userRoomsReserved.jsp").forward(request, response);
+		RoomServices.returnReservation(Integer.parseInt(request.getParameter("room_id")),
+				Integer.parseInt((request.getSession(false).getAttribute("accountID").toString())));
+
+		RequestDispatcher rd = request.getRequestDispatcher("ViewRoomsReserved");
+
+		rd.forward(request, response);
 	}
 
 }
